2afc63c5e3b27ded5e1e3c6b09f845fdbcd92a87
GNooNJo <jjjchoon@nate.com>
2024-02-22 15:06:05 +0900

    feat: apply login func

--- src/pages/non-auth/LoginPage.jsx
+++ src/pages/non-auth/LoginPage.jsx
@@ -13,15 +13,35 @@ const LoginPage = () => {
       <h1>Login</h1>
       <p>Login page</p>
 
-      <form onSubmit={async (e) => {}}>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          try {
+            const { data } = await authApi.post("/login", {
+              id,
+              password,
+            });
+            console.log(data);
+            const { accessToken, userId, nickname } = data;
+            localStorage.setItem("accessToken", accessToken);
+            localStorage.setItem("userId", userId);
+            localStorage.setItem("nickname", nickname);
+            alert("로그인에 성공하였습니다. 메인 페이지로 이동할게요");
+            navigate("/");
+          } catch (error) {
+            console.log(error);
+            alert(error.response.data.message);
+          }
+        }}
+      >
         <div>
           <label htmlFor="id">id</label>
-          <input />
+          <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
         </div>
 
         <div>
           <label htmlFor="password">Password</label>
-          <input />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
 
         <button type="submit">Login</button>
