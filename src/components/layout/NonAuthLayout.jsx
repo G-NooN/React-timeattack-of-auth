49aeaed49d8cab181155947ed5baac35f79979a6
GNooNJo <jjjchoon@nate.com>
2024-02-22 15:40:22 +0900

    feat: apply conditional routing by loginState

--- src/components/layout/NonAuthLayout.jsx
+++ src/components/layout/NonAuthLayout.jsx
@@ -3,6 +3,13 @@ import { Navigate, Outlet } from "react-router-dom";
 import Navigation from "../Navigation";
 
 const NonAuthLayout = () => {
+  const loginState = !!localStorage.getItem("accessToken");
+
+  if (loginState) {
+    alert("이미 로그인 상태입니다");
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
       <h1>Non Auth Layout</h1>
